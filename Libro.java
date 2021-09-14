3eb9cf6f022c5b14538d1f5e73719c610181b953
Hector-CG <71041201+Hector-CG@users.noreply.github.com>
2021-09-14 11:51:01 -0500

    Add files via upload
    Añadí un archivo de java.

---
+++ Libro.java
@@ -0,0 +1,15 @@
+package U2_Practica2;
+
+public class Libro {
+
+    String titulo;
+    String autor;
+    int numctrl;
+
+    public Libro(String titulo, String autor, int num) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numctrl = num;
+    }
+
+}
